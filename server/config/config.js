3ae95f537b0e9b238273d8d3b254a5149d4ff788
Eden <gresseden@gmail.com>
2021-01-02 18:24:45 -0500

    commit 1000

--- server/config/config.js
+++ server/config/config.js
@@ -2,22 +2,22 @@
 process.env.PORT = process.env.PORT || 3000
 
 // /*************************** 
-// Entorno
+// Entorno Esta variable la establece heroku
 // ****************************/
-// process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 
 // /*************************** 
 // Base de datos
 // *****************************/
-// let urlDB
+let urlDB
 
-// if (process.env.NODE_ENV === 'dev') {
-//   urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-//   urlDB = 'mongodb + srv://eden-udemy-node:b1oFF21lkvpUul1m@cluster0.xku6e.mongodb.net/test'
-// }
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+  urlDB = 'mongodb+srv://eden-udemy-node:b1oFF21lkvpUul1m@cluster0.xku6e.mongodb.net/test'
+}
 
-// process.env.URLDB = urlDB
+process.env.URLDB = urlDB
 
 
--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const options = {
 // mongoose.connect('mongodb+srv://eden-udemy-node:b1oFF21lkvpUul1m@cluster0.xku6e.mongodb.net/test', options, (err, res) => {
 
 // Mongo Compass
-mongoose.connect('mongodb + srv://eden-udemy-node:b1oFF21lkvpUul1m@cluster0.xku6e.mongodb.net/test' , options, (err, res) => {
+mongoose.connect(process.env.URLDB  , options, (err, res) => {
   if (err) {
     throw err
   }
