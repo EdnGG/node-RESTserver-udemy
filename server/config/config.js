ed7e08aaa0b94128fe7c068922df22938f8515ef
Eden <gresseden@gmail.com>
2021-01-02 15:07:30 -0500

    hello33

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,23 @@
-// PORT
-process.env.PORT = process.env.PORT || 3000
+// // PORT
+// process.env.PORT = process.env.PORT || 3000
 
-/*************************** 
-Entorno
-****************************/
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+// /*************************** 
+// Entorno
+// ****************************/
+// process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 
-/*************************** 
-Base de datos
-*****************************/
-let urlDB
+// /*************************** 
+// Base de datos
+// *****************************/
+// let urlDB
 
-if (process.env.NODE_ENV === 'dev') {
-  urlDB = 'mongodb://localhost:27017/cafe'
-} else {
-  urlDB = 'mongodb + srv://eden-udemy-node:b1oFF21lkvpUul1m@cluster0.xku6e.mongodb.net/test'
-}
+// if (process.env.NODE_ENV === 'dev') {
+//   urlDB = 'mongodb://localhost:27017/cafe'
+// } else {
+//   urlDB = 'mongodb + srv://eden-udemy-node:b1oFF21lkvpUul1m@cluster0.xku6e.mongodb.net/test'
+// }
 
-process.env.URLDB = urlDB
+// process.env.URLDB = urlDB
 
 
